100458bca82d0343c2f8d64bc0da5b274faa7cd1
tjtnwhd <tjtnwhd4979@naver.com>
2024-04-11 15:30:52 +0900

    라우팅 구성 완료

---
+++ src/commons/pages/NotFound.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const NotFound = () => {
+    return <h1>페이지 없음..</h1>
+};
+export default React.memo(NotFound);
---
+++ src/member/pages/Join.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Join = () => {
+  return <h1>화원 가입페이지</h1>;
+};
+
+export default React.memo(Join);
