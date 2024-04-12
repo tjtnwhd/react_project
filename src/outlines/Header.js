7dc5e6ff05192bda31dab85ea1ec7ad78a6db0e5
tjtnwhd <tjtnwhd4979@naver.com>
2024-04-12 16:48:36 +0900

    수정

--- src/outlines/Header.js
+++ src/outlines/Header.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 const Header = () => {
-  return <header>헤더</header>;
+  const HeaderBox = styled.header`
+    .site-top {
+      background: #f8f8f8;
+      border-bottom: 1px solid #d5d5d5;
+    }
+  `;
+  const { t } = useTranslation();
+  return (
+    <HeaderBox>
+      <section className="site-top">
+        <div className="layout-width">
+          <Link to="/member/join">{t('회원가입')}</Link>
+          <Link to="/member/login">{t('로그인')}</Link>
+        </div>
+      </section>
+    </HeaderBox>
+  );
 };
 export default React.memo(Header);
