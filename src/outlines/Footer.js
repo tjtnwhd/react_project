64a1c32a48f536992ec5936ad4c7deb42ec27f48
tjtnwhd <tjtnwhd4979@naver.com>
2024-04-15 17:22:24 +0900

    오류 수정

--- src/outlines/Footer.js
+++ src/outlines/Footer.js
@@ -10,8 +10,4 @@ const Footer = () => {
   return <FooterBox></FooterBox>;
 };
 
-<<<<<<< HEAD
 export default React.memo(Footer);
-=======
-export default React.memo(Footer);
->>>>>>> 599b614c76f66a28af3037c145d660aa58758131
