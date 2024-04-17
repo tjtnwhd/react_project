c082d161a01058bffdcd7e59d72e66879e16ca5e
tjtnwhd <tjtnwhd4979@naver.com>
2024-04-17 17:53:40 +0900

    회원 가입 양식 검증, 메세지 공통 컴포넌트

--- src/member/pages/Join.js
+++ src/member/pages/Join.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import JoinForm from '../components/JoinForm';
+import JoinContainer from '../containers/JoinContainer';
 import { useTranslation } from 'react-i18next';
 import { OuterBox } from '../../commons/components/LayoutBox';
 import { MainTitle } from '../../commons/components/TitleBox';
@@ -16,7 +16,7 @@ const Join = () => {
       </Helmet>
       <OuterBox>
         <MainTitle>{t('회원가입')}</MainTitle>
-        <JoinForm />
+        <JoinContainer />
       </OuterBox>
     </>
   );
